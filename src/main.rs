7437bac89879b4ba48eece8c07190fce080c5915
Juniper <basil.squared1@gmail.com>
2026-07-05 18:45:15 -0500

    WIP: actually workling on the shitting. parsing function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use lofty;
+use lofty::ogg::VorbisComments;
+use lofty::read_from_path;
 use clap::Parser;
+use lofty::file::{TaggedFile, TaggedFileExt};
+use lofty::tag::{ItemKey, Tag, TagType};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -12,6 +16,37 @@ struct Args {
 
 }
 
+enum ParseResult {
+    Handled,
+    Skipped,
+}
+fn parse_file(file: &mut TaggedFile) -> Option<ParseResult> {
+    // this is my vector list, say hi
+    let fuck_you_murder_list: Vec<ItemKey> = vec![
+        "TIDAL_ALBUM_ID",
+        "TIDAL_ALBUM_URL",
+        "TIDAL_DATA",
+        "TIDAL_TRACK_ID",
+        "TIDAL_TRACK_URL",
+    
+    ]
+    .into_iter()
+    .filter_map(|k| ItemKey::from_key(TagType::Id3v2, k))
+    .collect();
+
+    
+    let mut result = ParseResult::Skipped;
+
+    for tag_type in [TagType::VorbisComments, TagType::Id3v2] {
+        // since unfortunately, f.tags_mut() doesnt exist, i gotta do it the manual way
+        if let Some(tag) = file.tag_mut(tag_type) {
+            // TODO: actually... strip the fucking tags?
+            result = ParseResult::Handled;
+        }
+    }
+
+    Some(result)
+}
 
 fn main() {
     let music_args = Args::parse();
@@ -24,6 +59,7 @@ fn main() {
 
         }
         (None, Some(f)) => {
+            println!("Scanning File!");
 
 
         }
